7102f05646d2ceedb5d6b9fd6cbd7a008aac087f
GuillaumeDebreuille <guillaume.debreuille@gmail.com>
2026-09-11 18:41:16 +0200

    feature/shop : Initial products page setup with image folder and sample photos

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductsController extends AbstractController
 {
     #[Route('/products', name: 'app_products')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+
+        $products = $productRepository->findAll();
+
         return $this->render('shop/products/index.html.twig', [
             'controller_name' => 'ProductsController',
+            'products' => $products,
         ]);
     }
 }
